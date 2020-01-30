d73f9d42c6b14fff23749ddc4e97acdff9d65e04
Christoph Gmeiner <christophgmeiner@Christophs-MBP.fritz.box>
2020-01-30 11:25:14 +0100

    03: while true

--- sbahnmuc03.py
+++ sbahnmuc03.py
@@ -6,51 +6,53 @@ import os
 import boto3
 import pickle
 
-config = configparser.ConfigParser()
-config.read("dwh.cfg")
+while True:
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['KEY']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['SECRET']
-
-s3 = boto3.resource('s3')
-
-with open("station", "rb") as f:
-    fileobj = pickle.load(f)
+    config = configparser.ConfigParser()
+    config.read("dwh.cfg")
     
-s = schiene.Schiene()
-
-statconns = fileobj[1]
-
-i = 0
-
-for conns in statconns:
+    os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['KEY']
+    os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['SECRET']
     
-    try:
+    s3 = boto3.resource('s3')
     
-        c = s.connections(conns[0], conns[1])
+    with open("station", "rb") as f:
+        fileobj = pickle.load(f)
         
-        for conn in c:
+    s = schiene.Schiene()
+    
+    statconns = fileobj[1]
+    
+    i = 0
+    
+    for conns in statconns:
+        
+        try:
+        
+            c = s.connections(conns[0], conns[1])
             
-            if "delay" in conn.keys():
-        
-                conn["date"] = str(datetime.date.today())
-                conn["_id"] = str(conn["date"]) + "_" + conn["departure"]
-                conn["timestamp"] = str(datetime.datetime.now())
-                conn["total_delay"] = (conn["delay"]["delay_departure"] 
-                    + conn["delay"]["delay_arrival"])
+            for conn in c:
                 
-                filename = ("DB_" + conn["_id"] + "_" + conns[0] + "_" 
-                            + conns[1] + ".json")
-                filename = filename.replace(":", "_")
-                filename = filename.replace(" ", "_")
-                
-                s3object = s3.Object("sbmd1db", filename)
-                
-                s3object.put(Body=(bytes(json.dumps(conn).encode('UTF-8'))))
-                print(i)
-                i += 1
-                
-
-    except:
-        print(str(i) + "failed at " + conns[0] + "-" + conns[1] + "!")
-        i += 1
+                if "delay" in conn.keys():
+            
+                    conn["date"] = str(datetime.date.today())
+                    conn["_id"] = str(conn["date"]) + "_" + conn["departure"]
+                    conn["timestamp"] = str(datetime.datetime.now())
+                    conn["total_delay"] = (conn["delay"]["delay_departure"] 
+                        + conn["delay"]["delay_arrival"])
+                    
+                    filename = ("DB_" + conn["_id"] + "_" + conns[0] + "_" 
+                                + conns[1] + ".json")
+                    filename = filename.replace(":", "_")
+                    filename = filename.replace(" ", "_")
+                    
+                    s3object = s3.Object("sbmd1db", filename)
+                    
+                    s3object.put(Body=(bytes(json.dumps(conn)\
+                                             .encode('UTF-8'))))
+                    i += 1
+                    
+    
+        except:
+            print(str(i) + "failed at " + conns[0] + "-" + conns[1] + "!")
+            i += 1
