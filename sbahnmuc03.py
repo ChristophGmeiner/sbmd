69d5b722a6f36f81f99cbd6dde55cb36b3044194
Christoph Gmeiner <christophgmeiner@Christophs-MacBook-Pro.local>
2020-01-31 09:26:13 +0100

    ignore: update, 03: dynamic message

--- sbahnmuc03.py
+++ sbahnmuc03.py
@@ -5,57 +5,86 @@ import configparser
 import os
 import boto3
 import pickle
+from pytictoc import TicToc
+from sys import stdout
 
-while True:
+t = TicToc()
 
-    config = configparser.ConfigParser()
-    config.read("dwh.cfg")
+t.tic()
+
+config = configparser.ConfigParser()
+config.read("dwh.cfg")
+
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['KEY']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['SECRET']
+
+s3 = boto3.resource('s3')
+
+with open("station", "rb") as f:
+    fileobj = pickle.load(f)
     
-    os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['KEY']
-    os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['SECRET']
+s = schiene.Schiene()
+
+statit = fileobj[1]
+
+i = 0
+for st in statit:
+    i +=1
     
-    s3 = boto3.resource('s3')
+allstat = i
+
+i = 0
+
+with open("station", "rb") as f:
+    fileobj2 = pickle.load(f)
+
+statconns = fileobj2[1]
+
+for conns in statconns:
     
-    with open("station", "rb") as f:
-        fileobj = pickle.load(f)
+    try:
+    
+        c = s.connections(conns[0], conns[1])
         
-    s = schiene.Schiene()
-    
-    statconns = fileobj[1]
-    
-    i = 0
-    
-    for conns in statconns:
-        
-        try:
-        
-            c = s.connections(conns[0], conns[1])
+        for conn in c:
             
-            for conn in c:
+            if "delay" in conn.keys():
+        
+                conn["date"] = str(datetime.date.today())
+                conn["_id"] = (str(conn["date"]) + "_" + conn["departure"]
+                               + "_" + conns[0] + "_" + conns[1])
+                conn["timestamp"] = str(datetime.datetime.now())
+                conn["total_delay"] = (conn["delay"]["delay_departure"] 
+                    + conn["delay"]["delay_arrival"])
+                conn["start"] = conns[0]
+                conn["end"] = conns[1]
                 
-                if "delay" in conn.keys():
-            
-                    conn["date"] = str(datetime.date.today())
-                    conn["_id"] = (str(conn["date"]) + "_" + conn["departure"]
-                                   + "_" + conns[0] + "_" + conns[1])
-                    conn["timestamp"] = str(datetime.datetime.now())
-                    conn["total_delay"] = (conn["delay"]["delay_departure"] 
-                        + conn["delay"]["delay_arrival"])
-                    conn["start"] = conns[0]
-                    conn["end"] = conns[1]
+                filename = ("DB_" + conn["_id"] + "_" + conns[0] + "_" 
+                            + conns[1] + ".json")
+                filename = filename.replace(":", "_")
+                filename = filename.replace(" ", "_")
+                
+                s3object = s3.Object("sbmd1db", filename)
+                
+                s3object.put(Body=(bytes(json.dumps(conn)\
+                                         .encode('UTF-8'))))
+                             
+                stdout.write("\r%d" % i + " of " +  str(allstat) 
+                                + " finshed!")
+                stdout.flush()
+                
+                i += 1
                     
-                    filename = ("DB_" + conn["_id"] + "_" + conns[0] + "_" 
-                                + conns[1] + ".json")
-                    filename = filename.replace(":", "_")
-                    filename = filename.replace(" ", "_")
-                    
-                    s3object = s3.Object("sbmd1db", filename)
-                    
-                    s3object.put(Body=(bytes(json.dumps(conn)\
-                                             .encode('UTF-8'))))
-                    i += 1
-                    
+
+    except:
+        
+        stdout.write("\r%d" % i + " of " +  str(allstat) 
+                                + " finished!")
+        stdout.flush()
+        
+        i += 1
+        
+        
     
-        except:
-            print(str(i) + "failed at " + conns[0] + "-" + conns[1] + "!")
-            i += 1
+    
+t.toc()
