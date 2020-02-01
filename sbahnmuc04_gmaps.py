6a107a4001d744196fd1176891baba488689d8f5
Christoph Gmeiner <christophgmeiner@Christophs-MacBook-Pro.local>
2020-02-01 20:46:43 +0100

    04: parallel processing and less stations

--- sbahnmuc04_gmaps.py
+++ sbahnmuc04_gmaps.py
@@ -3,8 +3,14 @@ from datetime import datetime
 import boto3
 import configparser
 import os
-import pickle
 import json
+from itertools import combinations
+import pytictoc
+import multiprocessing as mp
+
+t = pytictoc.TicToc()
+
+t.tic()
 
 config = configparser.ConfigParser()
 config.read("dwh.cfg")
@@ -20,45 +26,68 @@ with open('gapi.txt') as f:
     
 gmap = googlemaps.Client(key=api_key)
 
-with open("station", "rb") as f:
-    fileobj = pickle.load(f)
+maps_stats = ["München-Ost", "München-Pasing", "Fürstenfeldbruck", "Landshut", 
+              "Nürnberg", "Augsburg-Rathausplatz", "Rosenheim", 
+              "München-Marienplatz"]
 
-statconns = fileobj[1]
+statconns = combinations(maps_stats, 2)
 
-i = 0
+pool = mp.Pool(mp.cpu_count())
 
-for conns in statconns:
+def gmap_query(start, end):
+    '''
+    loads
+    '''
+    now = datetime.now()
+    nowstring = str(now).replace(":", "_").replace(" ", "_")\
+                .replace(".", "_").replace("-", "_")
+    
+    results = gmap.directions(start, end,
+                              mode="driving",
+                              departure_time=now)
+    
+    resdict = results[0]["legs"][0]
+    
+    if resdict["steps"]:
+        del resdict["steps"]
+    
+    
+    resdict["stat1"] = start
+    resdict["stat2"] = end
+        
+    filename = ("Gmap_" + nowstring + "_" + start + "_" + end 
+                        + ".json")
+    filename = filename.replace(":", "_")
+    filename = filename.replace(" ", "_")
+    
+    s3object = s3.Object("sbmd2gmap", filename)
+
+    s3object.put(Body=(bytes(json.dumps(resdict).encode('UTF-8'))))
+
+def gmap_query_all(c):
+    '''
+    runs all gmap queries for provided stations
+    c: iterable containing 2 elements for start and stop station
+    '''
     
     try:
+        
+       gmap_query(c[0], c[1])
+        
+    except Exception as e:
+        print("Error at first round for " + c)
+        print(e)
+        
+    try:
+        
+       gmap_query(c[1], c[0])
+        
+    except Exception as e:
+        print("Error at first round for " + c)
+        print(e)
+    
+pool.map(gmap_query_all, [co for co in statconns])
 
-        now = datetime.now()
-        nowstring = str(now).replace(":", "_").replace(" ", "_")\
-                    .replace(".", "_").replace("-", "_")
-        
-        results = gmap.directions(conns[0], conns[1],
-                                  mode="driving",
-                                  departure_time=now)
-        
-        resdict = results[0]["legs"][0]
-        del resdict["steps"]
-        
-        resdict["stat1"] = conns[0]
-        resdict["stat2"] = conns[1]
-            
-        filename = ("Gmap_" + nowstring + "_" + conns[0] + "_" + conns[1] 
-                            + ".json")
-        filename = filename.replace(":", "_")
-        filename = filename.replace(" ", "_")
-        
-        s3object = s3.Object("sbmd2gmap", filename)
+pool.close()
     
-        s3object.put(Body=(bytes(json.dumps(resdict).encode('UTF-8'))))
-        
-        i += 1
-        
-        
-    except:
-        print(str(i) + "failed at " + conns[0] + "-" + conns[1] + "!")
-        i += 1
-    
-    
+t.toc()
