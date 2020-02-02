2030c102cb5fb595ad48aa7ffd22e5ddd8b3c9d1
Christoph Gmeiner <christophgmeiner@christophs-mbp.home>
2020-02-02 12:42:10 +0100

    03: cred corrections

--- sbahnmuc03.py
+++ sbahnmuc03.py
@@ -2,15 +2,13 @@ import schiene
 import datetime
 import json
 import configparser
-import os
 import boto3
 import pickle
 from pytictoc import TicToc
 import multiprocessing as mp
 
-def load_train(start, end, s = schiene.Schiene(), s3 = boto3.resource('s3'),
-              s3key_p=os.environ['AWS_ACCESS_KEY_ID'], 
-              s3skey_p=os.environ['AWS_SECRET_ACCESS_KEY']):
+def load_train(start, end, s3key, s3skey, 
+               s = schiene.Schiene()):
     '''
     loads connection details from a Schiene object
     start: start of the DB train connection, has to be a string and match a 
@@ -18,10 +16,6 @@ def load_train(start, end, s = schiene.Schiene(), s3 = boto3.resource('s3'),
     end: end of the DB train connection, has to be a string and match a 
          name of Schiene stations
     '''
-    
-    os.environ['AWS_ACCESS_KEY_ID'] = s3key_p
-    os.environ['AWS_SECRET_ACCESS_KEY'] = s3skey_p
-    
     c = s.connections(start, end)
     
     for conn in c:
@@ -41,26 +35,31 @@ def load_train(start, end, s = schiene.Schiene(), s3 = boto3.resource('s3'),
             filename = filename.replace(":", "_")
             filename = filename.replace(" ", "_")
             
+            s3 = boto3.resource('s3',
+                                aws_access_key_id=s3k,
+                                aws_secret_access_key= s3skey)
+            
             s3object = s3.Object("sbmd1db2", filename)
             
             s3object.put(Body=(bytes(json.dumps(conn)\
                                      .encode('utf-8'))))
 
-def load_trains_all(conns):
+def load_trains_all(conns, s3key_p, s3skey_p):
     '''
     runs all load_train queries for provided stations
     conns: iterable containing 2 elements for start and end station
     '''
-    
+
     try:
-        load_train(conns[0], conns[1])
+        load_train(conns[0], conns[1], s3key_p, s3skey_p)
     
     except Exception as e:
         print("Error at first round for " + conns[0] + "_" + conns[1])
         print(e)
         
+        
     try:
-        load_train(conns[1], conns[0])
+        load_train(conns[1], conns[0], s3key_p, s3skey_p)
     
     except Exception as e:
         print("Error at second round for " + conns[0] + "_" + conns[1])
@@ -82,12 +81,15 @@ def main():
     config = configparser.ConfigParser()
     config.read(credfile)
 
-    os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['KEY']
-    os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['SECRET']
+    config = configparser.ConfigParser()
+    config.read(credfile)
+
+    s3k = config['AWS']['KEY']
+    s3ks = config['AWS']['SECRET']
             
     pool = mp.Pool(mp.cpu_count())
     
-    pool.map(load_trains_all, [co for co in statit])
+    [pool.apply(load_trains_all, args=(co, s3k, s3ks)) for co in statit]
             
     t.toc()
     
