589c7506bbfb441d3014e3b42f896c5ca51fd343
ChristophGmeiner <christoph.gmeiner@gmail.com>
2020-02-11 10:52:22 +0100

    zz05, zz06: Initial

---
+++ zz05_StopRDS.py
@@ -0,0 +1,15 @@
+import boto3
+import configparser
+import os
+
+client = boto3.client("rds", region_name="eu-central-1")
+
+config = configparser.ConfigParser()
+config.read("/home/ec2-user/sbmd/dwh.cfg")
+
+rdsid = config['RDS']['ID1']
+
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['KEY']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['SECRET']
+
+response = client.stop_db_instance(DBInstanceIdentifier=rdsid)
