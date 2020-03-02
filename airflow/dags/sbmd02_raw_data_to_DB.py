1f2597b30beee101ba93242a8adfb0f3752b0b53
ChristophGmeiner <christoph.gmeiner@gmail.com>
2020-03-02 10:21:35 +0100

    dag02: Corr

--- airflow/dags/sbmd02_raw_data_to_DB.py
+++ airflow/dags/sbmd02_raw_data_to_DB.py
@@ -17,7 +17,7 @@ default_args = {
 
 dag = DAG("sbmd02_raw data_to_DB",
           description="Creates DBs and loads raw data from S3 to Postgres DB",
-          args=default_args,
+          default_args=default_args,
           schedule_interval="* * * * 1")
 
 create_DB_task = BashOperator(
