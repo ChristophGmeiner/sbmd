f76d7373aed32166036522c07b2647ff86152d86
Ubuntu <ubuntu@ip-172-31-42-147.eu-central-1.compute.internal>
2020-03-05 16:28:49 +0000

    dag01: New Scheduling

--- airflow/dags/sbmd01_web_data_gathering.py
+++ airflow/dags/sbmd01_web_data_gathering.py
@@ -18,7 +18,7 @@ default_args = {
 dag = DAG("sbmd01_web_data_gathering",
           description="Gathers all necessary web data",
           default_args=default_args,
-          schedule_interval="0 * * * *",
+          schedule_interval="0 3-23 * * *",
           max_active_runs=1)
 
 create_stations_task = BashOperator(
