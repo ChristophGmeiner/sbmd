4ed2bb8f9e49cbd618a561b6e05f8a47ab049fe4
Ubuntu <ubuntu@ip-172-31-42-147.eu-central-1.compute.internal>
2020-03-06 20:37:41 +0000

    copy airflow: Update

--- zz_copy_airflow.sh
+++ zz_copy_airflow.sh
@@ -1,3 +1,3 @@
-cp -rf /home/ec2-user/sbmd/airflow/dags /home/ec2-user/airflow
-cp -rf /home/ec2-user/sbmd/airflow/plugins /home/ec2-user/airflow
+cp -rf /home/ubuntu/sbmd/airflow/dags /home/ubuntu/airflow
+#cp -rf /home/ec2-user/sbmd/airflow/plugins /home/ec2-user/airflow
 
