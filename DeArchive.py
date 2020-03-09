1aa4862924e847d97fa96db0a473d18bf02a0fe8
ChristophGmeiner <christoph.gmeiner@gmail.com>
2020-03-09 11:56:32 +0100

    03b, 04b, 95b: Dearchive in error case

--- DeArchive.py
+++ DeArchive.py
@@ -2,42 +2,43 @@ import boto3
 import os
 import configparser
 
-cfg = "/home/ec2-user/sbmd/dwh.cfg"
-
-config = configparser.ConfigParser()
-config.read(cfg)
-
-rdsid = config['RDS']['ID1']
-rdspw = config["RDS"]["PW"]
-
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['KEY']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['SECRET']
-
-s3res = boto3.resource("s3")
-
-###Enter relevant bucket below
-s3r = boto3.resource("s3")
-BUCKET = "sbmd1db2"
-bucket = s3r.Bucket(BUCKET)
-objsr_all = bucket.objects.all()
-client = boto3.client("s3")
-
-###Enter relevant archive folder below
-archivname = "2020-03-03-ArchivDB/"
-
-s3r_files = []
-for o in objsr_all:
-    if o.key.find(archivname) > -1:
-        s3r_files.append(o.key)
-
-s3r_files = s3r_files[1:]
-
-###Enter specific index for only gathering the filename 
-###as key below
-for file in s3r_files[1:]:
-    #archiving back
-    copy_source = {"Bucket": BUCKET, "Key": file}
-    dest = s3res.Object(BUCKET, file[20:])
-    dest.copy(CopySource=copy_source)
-    response = s3res.Object(BUCKET, file).delete()
+def dearchive(BUCKET,
+              archivname,
+              index,
+              cfg="/home/ubuntu/sbmd/dwh.cfg"):
+    '''
+    dearchives failed archives
+    :BUCKET: relevant S3 bucket
+    :archivname: Relevant archivfolder, make sure to have a "/" at the end
+    :index: where does the file name start,
+            train: 20,
+            gmap: 22,
+            weather: 25
+    '''
+   
+    config = configparser.ConfigParser()
+    config.read(cfg)
+    
+    os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['KEY']
+    os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['SECRET']
+    
+    s3res = boto3.resource("s3")
+    
+    s3r = boto3.resource("s3")
+    bucket = s3r.Bucket(BUCKET)
+    objsr_all = bucket.objects.all()
+    
+    s3r_files = []
+    for o in objsr_all:
+        if o.key.find(archivname) > -1:
+            s3r_files.append(o.key)
+    
+    s3r_files = s3r_files[1:]
+    
+    for file in s3r_files[1:]:
+        #archiving back
+        copy_source = {"Bucket": BUCKET, "Key": file}
+        dest = s3res.Object(BUCKET, file[index:])
+        dest.copy(CopySource=copy_source)
+        response = s3res.Object(BUCKET, file).delete()
 
--- sbahnmuc03b_TransferDB.py
+++ sbahnmuc03b_TransferDB.py
@@ -7,6 +7,7 @@ import json
 import pytictoc
 import datetime
 import logging
+from DeArchive import dearchive
 
 t = pytictoc.TicToc()
 t.tic()
@@ -33,65 +34,75 @@ client = boto3.client("s3")
 
 logging.info("Starting gather S3 files...")
 
-s3r_files = []
-for o in objsr_all:
-    s3r_files.append(o.key)
+try:
 
-s3r_files = [x for x in s3r_files if x.find("/") == -1]
-
-logging.info("Finished gathering S3 files.")
-
-basefile = s3r_files[0]
-result = client.get_object(Bucket=BUCKET, Key=basefile) 
-text = json.loads(result["Body"].read().decode())
-base_df = pd.io.json.json_normalize(text, sep="_")
-
-logging.info("Finished DF")
-
-FILE_TO_READ = s3r_files[0]
-client = boto3.client('s3')
-archivfoldername = str(datetime.date.today()) + "-ArchivDB/"
-response = client.put_object(
-        Bucket=BUCKET,
-        Body="",
-        Key=archivfoldername)
-s3res = boto3.resource("s3")
-copy_source = {"Bucket": BUCKET, "Key": s3r_files[0]}
-dest = s3res.Object(BUCKET, archivfoldername + copy_source["Key"])
-dest.copy(CopySource=copy_source)
-response = s3res.Object(BUCKET, s3r_files[0]).delete()
-
-logging.info("Finished first line of df")
-
-for file in s3r_files[1:]:
-    result = client.get_object(Bucket=BUCKET, Key=file) 
-    text = json.loads(result["Body"].read().decode())
-    df = pd.io.json.json_normalize(text, sep="_")
-    base_df = pd.concat([base_df, df], axis=0, ignore_index=True)
+    s3r_files = []
+    for o in objsr_all:
+        s3r_files.append(o.key)
     
-    #archiving
-    copy_source = {"Bucket": BUCKET, "Key": file}
+    s3r_files = [x for x in s3r_files if x.find("/") == -1]
+    
+    logging.info("Finished gathering S3 files.")
+    
+    basefile = s3r_files[0]
+    result = client.get_object(Bucket=BUCKET, Key=basefile) 
+    text = json.loads(result["Body"].read().decode())
+    base_df = pd.io.json.json_normalize(text, sep="_")
+    
+    logging.info("Finished DF")
+    
+    FILE_TO_READ = s3r_files[0]
+    client = boto3.client('s3')
+    archivfoldername = str(datetime.date.today()) + "-ArchivDB/"
+    response = client.put_object(
+            Bucket=BUCKET,
+            Body="",
+            Key=archivfoldername)
+    s3res = boto3.resource("s3")
+    copy_source = {"Bucket": BUCKET, "Key": s3r_files[0]}
     dest = s3res.Object(BUCKET, archivfoldername + copy_source["Key"])
     dest.copy(CopySource=copy_source)
-    response = s3res.Object(BUCKET, file).delete()
-
-logging.info("Finished whole DF and file")
-
-coln = list(base_df.columns)
-coln = [x.lower() for x in coln]
-base_df.columns = coln
-
-base_df_filename = str(datetime.date.today()) + "_DB_DF.csv"
-base_df.to_csv("/home/ubuntu/sbmd/" + base_df_filename, index=False)
-
-s3object = s3res.Object(BUCKET, "CSVs/" + base_df_filename)
-s3object.upload_file("/home/ubuntu/sbmd/" + base_df_filename)
-os.remove("/home/ubuntu/sbmd/" + base_df_filename)
-
-logging.info("Finished Copy, starting data quality checks")
-
-if (len(s3r_files) != base_df.shape[0]):
-    logging.error("Data Quality check failed! Files and DF length not \
-                  identical!")
+    response = s3res.Object(BUCKET, s3r_files[0]).delete()
+    
+    logging.info("Finished first line of df")
+    
+    for file in s3r_files[1:]:
+        result = client.get_object(Bucket=BUCKET, Key=file) 
+        text = json.loads(result["Body"].read().decode())
+        df = pd.io.json.json_normalize(text, sep="_")
+        base_df = pd.concat([base_df, df], axis=0, ignore_index=True)
+        
+        #archiving
+        copy_source = {"Bucket": BUCKET, "Key": file}
+        dest = s3res.Object(BUCKET, archivfoldername + copy_source["Key"])
+        dest.copy(CopySource=copy_source)
+        response = s3res.Object(BUCKET, file).delete()
+    
+    logging.info("Finished whole DF and file")
+    
+    coln = list(base_df.columns)
+    coln = [x.lower() for x in coln]
+    base_df.columns = coln
+    
+    base_df_filename = str(datetime.date.today()) + "_DB_DF.csv"
+    base_df.to_csv("/home/ubuntu/sbmd/" + base_df_filename, index=False)
+    
+    s3object = s3res.Object(BUCKET, "CSVs/" + base_df_filename)
+    s3object.upload_file("/home/ubuntu/sbmd/" + base_df_filename)
+    os.remove("/home/ubuntu/sbmd/" + base_df_filename)
+    
+    logging.info("Finished Copy, starting data quality checks")
+    
+    if (len(s3r_files) != base_df.shape[0]):
+        logging.error("Data Quality check failed! Files and DF length not \
+                      identical!")
+        
+except Exception as e:
+    logging.error("Something went wrong...startind de-archiving!")
+    logging.error(e)
+    
+    dearchive(BUCKET, archivfoldername, 20)
+    
+    logging.info("Succesfully dearchived!")
 
 t.toc()
