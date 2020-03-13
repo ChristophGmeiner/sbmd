44c8e4d32e91fa6c5265ded87e934f25dd78ceef
ChristophGmeiner <christoph.gmeiner@gmail.com>
2020-03-13 11:34:31 +0100

    Create, Insert: new table structure

--- CreateTables.py
+++ CreateTables.py
@@ -26,81 +26,83 @@ class CreateTables:
                     start_loc VARCHAR NOT NULL, end_loc VARCHAR NOT NULL, 
                     timestamp TIMESTAMP NOT NULL, departure TIMESTAMP NOT NULL, 
                     arrival TIMESTAMP NOT NULL, transfers INT, 
-                    total_time TIMESTAMP NOT NULL, products VARCHAR, \
-                    price FLOAT, ontime BOOLEAN NOT NULL, 
+                    total_time VARCHAR NOT NULL, products VARCHAR, \
+                    price VARCHAR, ontime BOOLEAN NOT NULL, 
                     cancelled BOOLEAN NOT NULL, total_delay INT NOT NULL, 
                     delay_departure INT NOT NULL, 
                     delay_arrival INT NOT NULL, dep_date DATE)
                     """
     
     create_table1s = createsql + table1s + """ (details VARCHAR, 
-                    departure VARCHAR, arrival VARCHAR,	transfers INT, 
+                    departure VARCHAR, arrival VARCHAR,	transfers VARCHAR, 
                     time VARCHAR, products VARCHAR, 	price VARCHAR,
-                    ontime BOOLEAN,	 canceled BOOLEAN, "date" DATE, 
-                    _id VARCHAR, "timestamp" TIMESTAMP, 	total_delay FLOAT, 
+                    ontime VARCHAR,	 canceled VARCHAR, "date" VARCHAR, 
+                    _id VARCHAR, "timestamp" VARCHAR, total_delay VARCHAR, 
                     "start" VARCHAR, "end" VARCHAR, 
-                    delay_delay_departure FLOAT,
-                    delay_delay_arrival FLOAT)
+                    delay_delay_departure VARCHAR,
+                    delay_delay_arrival VARCHAR)
                     """
      
     create_table2 = createsql + table2 + """ (gmap_id INT IDENTITY(0, 1) 
                     PRIMARY KEY SORTKEY, 
-                    "start" VARCHAR NOT NULL, "end_loc" VARCHAR NOT NULL, 
-                    timestamp TIMESTAMP NOT NULL, start_loc_lat FLOAT, 
-                    start_loc_lng FLOAT, start_adress VARCHAR, 
-                    end_loc_lat FLOAT, end_loc_lng FLOAT, end_adress VARCHAR, 
-                    distance_text VARCHAR, distance_value FLOAT, 
-                    duration_text VARCHAR, duration_value FLOAT, 
-                    duration_traffic_text VARCHAR, 
-                    duration_traffic_value FLOAT)
+                    start_loc VARCHAR NOT NULL, end_loc VARCHAR NOT NULL, 
+                    timestamp TIMESTAMP NOT NULL, start_loc_lat VARCHAR, 
+                    start_loc_lng VARCHAR, start_adress VARCHAR, 
+                    end_loc_lat VARCHAR, end_loc_lng VARCHAR, 
+                    end_adress VARCHAR, distance_text VARCHAR, 
+                    distance_value INT, duration_text VARCHAR, 
+                    duration_value INT, duration_traffic_text VARCHAR, 
+                    duration_traffic_value INT)
                     """
     
     create_table2s = createsql + table2s + """ (end_address VARCHAR,
                     	start_address VARCHAR, traffic_speed_entry VARCHAR,
                     via_waypoint VARCHAR, stat1 VARCHAR, stat2 VARCHAR, 
-                    "timestamp" TIMESTAMP, distance_text VARCHAR , 
-                    distance_value FLOAT, duration_text VARCHAR, 
-                    duration_value FLOAT, duration_in_traffic_text VARCHAR, 
-                    duration_in_traffic_value FLOAT, end_location_lat VARCHAR,
-                    end_location_lng VARCHAR, start_location_lat VARCHAR, 
-                    start_location_lng VARCHAR)
+                    "timestamp" VARCHAR, distance_text VARCHAR , 
+                    distance_value VARCHAR, duration_text VARCHAR, 
+                    duration_value VARCHAR, duration_in_traffic_text VARCHAR, 
+                    duration_in_traffic_value VARCHAR, 
+                    end_location_lat VARCHAR, end_location_lng VARCHAR, 
+                    start_location_lat VARCHAR, start_location_lng VARCHAR)
                     """
     
     create_table3 = createsql + table3 + """ (weather_id INT IDENTITY(0, 1) 
                    PRIMARY KEY SORTKEY, 
-                   loc_name VARCHAR NOT NULL, loc_id FLOAT NULL, 
-                   clouds FLOAT, detailed_status VARCHAR, dewpoint VARCHAR, 
-                   heat_index VARCHAR, humidex VARCHAR, humidity FLOAT, 
-                   press FLOAT, sea_level VARCHAR, rain_1h FLOAT, 
-                   rain_3h FLOAT, reference_time FLOAT NOT NULL, 
+                   loc_name VARCHAR NOT NULL, loc_id BIGINT NULL, 
+                   clouds INT, detailed_status VARCHAR, dewpoint VARCHAR, 
+                   heat_index VARCHAR, humidex VARCHAR, humidity INT, 
+                   press INT, sea_level INT, rain_1h FLOAT, 
+                   rain_3h FLOAT, reference_time TIMESTAMP NOT NULL, 
                    snow_1h FLOAT, snow_3h FLOAT, status VARCHAR, 
-                   sunrise_time FLOAT, sunset_time FLOAT, 
+                   sunrise_time TIMESTAMP, sunset_time TIMESTAMP, 
                    temperature FLOAT, temperature_kf FLOAT, 
                    temperature_max FLOAT, temperature_min FLOAT, 
-                   visibility_distance FLOAT, weather_code INT, 
+                   visibility_distance INT, weather_code INT, 
                    weather_iconname VARCHAR, wind_deg FLOAT, wind_gust FLOAT, 
-                   wind_speed FLOAT, reception_time FLOAT NOT NULL)
+                   wind_speed FLOAT, reception_time TIMESTAMP NOT NULL)
                    """
                    
-    create_table3s = createsql + table3s + """ (location_id BIGINT, 
+    create_table3s = createsql + table3s + """ (location_id VARCHAR, 
                   location_coordinates_lat VARCHAR, 
                   location_coordinates_lon VARCHAR, location_country VARCHAR,
-                  location_name VARCHAR, weather_clouds FLOAT, 
+                  location_name VARCHAR, weather_clouds VARCHAR, 
                   weather_detailed_status VARCHAR, weather_dewpoint VARCHAR,
                   weather_heat_index VARCHAR, weather_humidex VARCHAR, 
-                  weather_humidity FLOAT, weather_pressure_press FLOAT, 
+                  weather_humidity VARCHAR, weather_pressure_press VARCHAR, 
                   weather_pressure_sea_level VARCHAR, weather_rain_1h VARCHAR,
-                  weather_rain_3h VARCHAR, weather_reference_time BIGINT, 
+                  weather_rain_3h VARCHAR, weather_reference_time VARCHAR, 
                   weather_snow_1h VARCHAR, weather_snow_3h VARCHAR, 
-                  weather_status VARCHAR, weather_sunrise_time BIGINT,
-                  weather_sunset_time BIGINT, weather_temperature_temp VARCHAR,
+                  weather_status VARCHAR, weather_sunrise_time VARCHAR,
+                  weather_sunset_time VARCHAR, 
+                  weather_temperature_temp VARCHAR,
                   weather_temperature_temp_kf VARCHAR, 
                   weather_temperature_temp_max VARCHAR, 
                   weather_temperature_temp_min VARCHAR,
-                  weather_visibility_distance FLOAT, 
-                  weather_weather_code INT, 	weather_weather_icon_name VARCHAR,
+                  weather_visibility_distance VARCHAR, 
+                  weather_weather_code VARCHAR, 
+                  weather_weather_icon_name VARCHAR,
                   weather_wind_deg VARCHAR,	weather_wind_gust VARCHAR,
-                  weather_wind_speed VARCHAR, reception_time BIGINT)
+                  weather_wind_speed VARCHAR, reception_time VARCHAR)
                    """
 
     
--- airflow/plugins/helpers/InsertTables.py
+++ airflow/plugins/helpers/InsertTables.py
@@ -1,15 +1,21 @@
 class InsertTables:
 
     delsql1 = """
+              DELETE FROM t_db01_stagings
+              WHERE details = 'details';
+    
               DELETE FROM t_db01_live
                 WHERE departure || '_' || start_loc || '_' || end_loc
                  IN
                  (SELECT DISTINCT
-                 TO_TIMESTAMP(departure, 'HH24:MI')::TIME || '_' || "start" || '_' || "end"
+                 CAST("date" || ' ' || departure AS TIMESTAMP) || '_' || "start" || '_' || "end"
                  FROM t_db01_stagings);
               """
     
     delsql2 = """
+                DELETE FROM t_gmap01_stagings
+                WHERE end_address = 'end_address';
+    
                 DELETE FROM t_gmap01_live
                 WHERE CAST("timestamp" AS TIMESTAMP) || '_' || start_loc 
                 || '_' || end_loc
@@ -21,6 +27,9 @@ class InsertTables:
               """
     
     delsql3 = """
+              DELETE FROM t_w01_stagings
+              WHERE location_id = 'location_id';
+            
               DELETE FROM t_w01_live
               WHERE reception_time || '_' || loc_name IN
                   (SELECT DISTINCT 
@@ -29,77 +38,77 @@ class InsertTables:
               t_w01_stagings);
               """
     inssql1 = """
-            INSERT INTO t_db01_live(
-                start_loc, 
-                end_loc, 
-                "timestamp", 
-                departure, 
-                arrival, 
-                transfers, 
-                total_time, 
-                products, 
-                price, 
-                ontime, 
-                cancelled, 
-                total_delay, 
-                delay_departure, 
-                delay_arrival,
-                dep_date)
-            SELECT DISTINCT
-                "start",
-                "end",
-                CAST("timestamp" AS TIMESTAMP),
-                TO_TIMESTAMP("date" || ' ' || departure, 'YYYY-MM-DD HH24:MI')::TIME AS departure,
-                TO_TIMESTAMP("date" || ' ' || arrival, 'YYYY-MM-DD HH24:MI')::TIME AS arrival,
-                transfers,
-                TO_TIMESTAMP(arrival, 'HH24:MI')::TIME - TO_TIMESTAMP(departure, 'HH24:MI')::TIME,
-                products,
-                CAST(price AS FLOAT),
-                ontime,
-                canceled,
-                total_delay,
-                delay_delay_departure,
-                delay_delay_arrival,
-                CAST("date" AS DATE)
-            FROM
-            t_db01_stagings;
+                INSERT INTO t_db01_live(
+                    start_loc, 
+                    end_loc, 
+                    "timestamp", 
+                    departure, 
+                    arrival, 
+                    transfers, 
+                    total_time, 
+                    products, 
+                    price, 
+                    ontime, 
+                    cancelled, 
+                    total_delay, 
+                    delay_departure, 
+                    delay_arrival,
+                    dep_date)
+                SELECT DISTINCT
+                    "start",
+                    "end",
+                    CAST("timestamp" AS TIMESTAMP),
+                    CAST("date" || ' ' || departure AS TIMESTAMP) AS departure,
+                    CAST("date" || ' ' || arrival AS TIMESTAMP) AS arrival,
+                    CAST(transfers AS INT),
+                    CAST("date" || ' ' || arrival AS TIMESTAMP) -  CAST("date" || ' ' || departure AS TIMESTAMP),
+                    products,
+                    price,
+                    CASE WHEN ontime = 'True' THEN 1 ELSE 0 END, 
+                    CASE WHEN canceled = 'True' THEN 1 ELSE 0 END,
+                    CAST(total_delay AS INT),
+                    CAST(delay_delay_departure AS INT),
+                    CAST(delay_delay_arrival AS INT),
+                    CAST("date" AS DATE)
+                FROM
+                t_db01_stagings;
              """
     
     inssql2 = """
-             INSERT INTO t_gmap01_live(
-                start_loc, 
-                end_loc, 
-                "timestamp", 
-                start_loc_lat, 
-                start_loc_lng, 
-                start_adress, 
-                end_loc_lat, 
-                end_loc_lng, 
-                end_adress, 
-                distance_text, 
-                distance_value, 
-                duration_text, 
-                duration_value, 
-                duration_in_traffic_text, 
-                duration_in_traffic_value
-            SELECT DISTINCT
-                stat1,
-                stat2,
-                CAST("timestamp" AS TIMESTAMP),
-                start_location_lat,
-                start_location_lng,
-                start_address,
-                end_location_lat,
-                end_location_lat,
-                end_address,
-                distance_text,
-                distance_value,
-                duration_text,
-                duration_value,
-                duration_in_traffic_text, 
-                duration_in_traffic_value
-            FROM t_gmap01_stagings
-            WHERE "timestamp" IS NOT NULL;
+              INSERT INTO t_gmap01_live(
+                    start_loc, 
+                    end_loc, 
+                    "timestamp", 
+                    start_loc_lat, 
+                    start_loc_lng, 
+                    start_adress, 
+                    end_loc_lat, 
+                    end_loc_lng, 
+                    end_adress, 
+                    distance_text, 
+                    distance_value, 
+                    duration_text, 
+                    duration_value, 
+                    duration_traffic_text, 
+                    duration_traffic_value)
+                SELECT DISTINCT
+                    stat1,
+                    stat2,
+                    CAST("timestamp" AS TIMESTAMP),
+                    start_location_lat,
+                    start_location_lng,
+                    start_address,
+                    end_location_lat,
+                    end_location_lat,
+                    end_address,
+                    distance_text,
+                    CAST(distance_value AS INT),
+                    duration_text,
+                    CAST(duration_value AS INT),
+                    duration_in_traffic_text, 
+                    CAST(duration_in_traffic_value AS INT)
+                FROM t_gmap01_stagings
+                WHERE "timestamp" IS NOT NULL;
                 """
     
     inssql3 = """
@@ -133,37 +142,37 @@ class InsertTables:
                      wind_gust, 
                      wind_speed, 
                      reception_time) 
-                SELECT
-                    location_name,
-                    location_id, 
-                    weather_clouds, 
-                    weather_detailed_status, 
-                    weather_dewpoint, 
-                    weather_heat_index, 
-                    weather_humidex, 
-                    weather_humidity, 
-                    weather_pressure_press, 
-                    weather_pressure_sea_level, 
-                    weather_rain_1h, 
-                    weather_rain_3h, 
-                    weather_reference_time, 
-                    weather_snow_1h, 
-                    weather_snow_3h, 
-                    weather_status, 
-                    weather_sunrise_time, 
-                    weather_sunset_time, 
-                    weather_temperature_temp, 
-                    CAST (weather_temperature_temp_kf AS FLOAT), 
-                    weather_temperature_temp_max, 
-                    weather_temperature_temp_min, 
-                    weather_visibility_distance, 
-                    weather_weather_code, 
-                    weather_weather_icon_name, 
-                    weather_wind_deg, 
-                    weather_wind_gust, 
-                    weather_wind_speed, 
-                    reception_time
-                    FROM t_w01_stagings;
+                SELECT DISTINCT
+                        location_name,
+                        CAST(location_id AS BIGINT), 
+                        CASE WHEN weather_clouds = ' ' THEN NULL ELSE CAST(weather_clouds AS INT) END, 
+                        weather_detailed_status, 
+                        weather_dewpoint, 
+                        weather_heat_index, 
+                        weather_humidex, 
+                        CASE WHEN weather_humidity = ' ' THEN NULL ELSE CAST(weather_humidity AS INT) END,
+                        CASE WHEN weather_pressure_press = ' ' THEN NULL ELSE CAST(weather_pressure_press AS INT) END, 
+                        CASE WHEN weather_pressure_sea_level = ' ' THEN NULL ELSE CAST(weather_pressure_sea_level AS INT) END,
+                        CASE WHEN weather_rain_1h = '' THEN NULL ELSE CAST(weather_rain_1h AS FLOAT) END,
+                        CASE WHEN weather_rain_3h = '' THEN NULL ELSE CAST(weather_rain_3h AS FLOAT) END,
+                        TIMESTAMP 'epoch' + weather_reference_time/1 * INTERVAL '1 second', 
+                        CASE WHEN weather_snow_1h = '' THEN NULL ELSE CAST(weather_snow_1h AS FLOAT) END,
+                        CASE WHEN weather_snow_3h = '' THEN NULL ELSE CAST(weather_snow_3h AS FLOAT) END,
+                        weather_status, 
+                        TIMESTAMP 'epoch' + weather_sunrise_time/1 * INTERVAL '1 second',
+                        TIMESTAMP 'epoch' + weather_sunset_time/1 * INTERVAL '1 second', 
+                        CASE WHEN weather_temperature_temp = '' THEN NULL ELSE CAST(weather_temperature_temp AS FLOAT) END,
+                        CASE WHEN weather_temperature_temp_kf = '' THEN NULL ELSE CAST(weather_temperature_temp_kf AS FLOAT) END, 
+                        CASE WHEN weather_temperature_temp_max = '' THEN NULL ELSE CAST(weather_temperature_temp_max AS FLOAT) END, 
+                        CASE WHEN weather_temperature_temp_min = '' THEN NULL ELSE CAST(weather_temperature_temp_min AS FLOAT) END, 
+                        CASE WHEN weather_visibility_distance = ' ' THEN NULL ELSE CAST(weather_visibility_distance AS INT) END, 
+                        CASE WHEN weather_weather_code = ' ' THEN 0 ELSE CAST(weather_weather_code AS INT) END, 
+                        weather_weather_icon_name, 
+                        CASE WHEN weather_wind_deg = '' THEN NULL ELSE CAST(weather_wind_deg AS FLOAT) END, 
+                        CASE WHEN weather_wind_gust = '' THEN NULL ELSE CAST(weather_wind_gust AS FLOAT) END,
+                        CASE WHEN weather_wind_speed = '' THEN NULL ELSE CAST(weather_wind_speed AS FLOAT) END, 
+                        TIMESTAMP 'epoch' + reception_time/1 * INTERVAL '1 second'
+                        FROM t_w01_stagings;
                 """
     
     delsql_list = [delsql1, delsql2, delsql3]
