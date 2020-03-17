6d99d902cd25fdc15c4bad9d4596f81438babbf7
ChristophGmeiner <christoph.gmeiner@gmail.com>
2020-03-17 09:39:00 +0100

    op s3csvred: flexible colnames in Copy command

--- airflow/plugins/operators/s3csvredshift.py
+++ airflow/plugins/operators/s3csvredshift.py
@@ -2,12 +2,16 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from io import StringIO
+import pandas as pd
+import boto3
 
 class S3CSVToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
     
     copy_sql = """
                     COPY {}
+                    ({})
                     FROM '{}'
                     ACCESS_KEY_ID '{}'
                     SECRET_ACCESS_KEY '{}'
@@ -56,18 +60,50 @@ class S3CSVToRedshiftOperator(BaseOperator):
         creds = aws_hook.get_credentials()
         rs_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         
-        self.log.info("Copying data from S3 to Redshift for " + self.table)
-        s3_path = "s3://" + self.s3_bucket + "/" + self.s3_key
-        formated_sql = S3CSVToRedshiftOperator.copy_sql.format(
-                self.table,
-                s3_path,
-                creds.access_key,
-                creds.secret_key,
-                self.s3_region)
-              
-        rs_hook.run(formated_sql)
+        self.log.info("Starting gathering CSV files from S3...")
         
-        self.log.info('StageToRedshiftOperator finished!')
+        s3r = boto3.resource("s3", 
+                             region_name=self.s3_region, 
+                             aws_access_key_id=creds.access_key, 
+                             aws_secret_access_key=creds.secret_key)
+        bucket = s3r.Bucket(self.s3_bucket)
+        csv_obj = bucket.objects.filter(Prefix=self.s3_key)
+        csvlist = []
+        for c in csv_obj:
+            csvlist.append(c.key)
+        csvlist = [x for x in csvlist if x.find(".csv") > -1]
+        
+        client = boto3.client("s3", 
+                             region_name=self.s3_region, 
+                             aws_access_key_id=creds.access_key, 
+                             aws_secret_access_key=creds.secret_key)
+                
+        i = 1
+        for c in csvlist:
+            ind = str(i)
+            csvo = client.get_object(Bucket=self.s3_bucket, Key=c)
+            body = csvo["Body"]
+            csv_string = body.read().decode("utf-8")
+            df = pd.read_csv(StringIO(csv_string))
+            cols = list(df.columns)
+            cols = ', '.join(cols)
+               
+            self.log.info(f"Copying file {ind} from S3 to Redshift for " 
+                          + self.table)
+            s3_path = "s3://" + self.s3_bucket + "/" + c
+            formated_sql = S3CSVToRedshiftOperator.copy_sql.format(
+                    self.table,
+                    cols,
+                    s3_path,
+                    creds.access_key,
+                    creds.secret_key,
+                    self.s3_region)
+              
+            rs_hook.run(formated_sql)
+        
+            self.log.info(f'StageToRedshiftOperator finished for file {ind}!')
+            
+            i += 1
         
         check_sql = f"SELECT COUNT(*) FROM {self.table}"
         
